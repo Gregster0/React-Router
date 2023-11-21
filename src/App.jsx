8fa6c4a5394a8e4717ece741f9b14d338a7b0807
Gregory Jean Baptiste <gregster0@gmail.com>
2023-11-21 12:45:04 -0500

    fully functional

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,8 @@
 import { useState } from 'react'
-import { Routes, Route } from "react-router-dom";
+import { Routes, Route, Link } from "react-router-dom";
+import Blue from './components/Blue.jsx';
+import Red from './components/Red.jsx';
+import Home from './components/Home.jsx';
 import './App.css'
 
 function App() {
@@ -7,10 +10,15 @@ function App() {
   return (
     <>
       <div id="container">
-        <div id="navbar">{/* navigation here */}</div>
+        <div id="navbar">{<><Link to='/'>Feelin' Homesick?</Link>
+                            <Link to='/blue'>Feelin' Blue?</Link>
+                            <Link to='/red'>Do you Bleed?</Link></>
+
+        }</div>
         <div id="main-section">{<Routes>
-                                    <Route path='/blue' element={<h1>Blue</h1>}/>
-                                    <Route path='/red' element={<h1>Red</h1>}/>
+                                    <Route path='/' element={<Home />}/>
+                                    <Route path='/blue' element={<Blue />}/>
+                                    <Route path='/red' element={<Red />}/>
                                 </Routes>}</div>
       </div>
     </>
